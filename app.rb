2c996d89f61e0a1f6b5ded3dc6f47c9b6f108e0b
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 20:23:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   get '/' do
        erb :user_input
      end
-   
+
      post '/piglatinize' do
        @text = PigLatinizer.new.to_pig_latin(params[:user_phrase])
        erb :piglatinize
